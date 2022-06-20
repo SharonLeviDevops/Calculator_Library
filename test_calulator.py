c014017c142fd26c3a1fafcdd094174b8f7d8622
Sharon.Levi <sharont55l@gmail.com>
2022-06-20 11:28:35 +0300

    fix whitespaces

--- test_calulator.py
+++ test_calulator.py
@@ -1,5 +1,5 @@
 """""
-Unit tests for the calulator library
+Unit tests for the calculator library
 """""
 
 import calculator
@@ -8,7 +8,7 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
-        assert  4 == calculator.add(2, 2)
+        assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
